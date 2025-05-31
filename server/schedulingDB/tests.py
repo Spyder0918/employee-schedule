1e042b6d35509286961a9b89439f06a86b7140d5
Spyder918 <jacobs.brandon918@gmail.com>
2025-05-31 10:30:52 -0400

    added a an api test case

--- server/schedulingDB/tests.py
+++ server/schedulingDB/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ShiftTests(APITestCase):
+    def test_get_shifts(self):
+        url = reverse('shifts-list')  # Or hardcode the URL: '/api/shifts/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
